e52d7c8d8daaea225e755e90cdfebf57c15ad175
sametfurkan <59727471+sametfurkan@users.noreply.github.com>
2024-08-11 01:35:52 +0300

    new

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -10,17 +10,19 @@ const Signin = () => {
         <form>
           <input type="email" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <label>
-            <input
-              type="checkbox"
-              name="rememberme"
-              placeholder="Remember me"
-            />
-            Remember me
-          </label>
-          <p>Forgot Password?</p>
-
-          <button>Login</button>
+          <div class="form-options">
+            <label>
+              <input type="checkbox" name="rememberme" />
+              <span>Remember me</span>
+            </label>
+            <a href="#" class="forgot-password">
+              Forgot password?
+            </a>
+          </div>
+          <button type="submit">Login</button>
+          <p>
+            Don't have an account? <a href="#">Register</a>
+          </p>
         </form>
       </div>
     </div>
